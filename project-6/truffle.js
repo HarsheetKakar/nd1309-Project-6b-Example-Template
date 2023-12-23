deeb2603874cddd766418c66d04116e9fe2e4a31
Harsheet Kakar <harsheetkakar@gmail.com>
2023-12-23 14:24:13 +0530

    added roles and updated solidity version

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -5,5 +5,11 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     }
+  },
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.6.12" // Fetch exact version from solc-bin
+    }
   }
 };
