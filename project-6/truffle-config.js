89aec57c3f858d9e0209a1852739e7721f08289f
Harsheet Kakar <harsheetkakar@gmail.com>
2024-01-01 19:18:34 +0530

    added readme file

--- project-6/truffle-config.js
+++ project-6/truffle-config.js
@@ -1,3 +1,14 @@
+
+const SEPOLIA_URL =
+  "https://sepolia.infura.io/v3/" + process.env.INFURA_PROJECT_ID;
+console.log(SEPOLIA_URL)
+
+const HDWallet = require("truffle-hdwallet-provider");
+// const infuraKey = "fj4jll3k.....";
+//
+const fs = require("fs");
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
   networks: {
     development: {
@@ -10,6 +21,15 @@ module.exports = {
       port: 8545,
       network_id: "*", // Match any network id
     },
+    sepolia: {
+      provider: function () {
+        return new HDWallet(mnemonic, SEPOLIA_URL);
+      },
+      gas: 8000000, // adjust the gas limit as needed
+      gasPrice: 20000000000, // set your gas price
+      networkCheckTimeout: 100000,
+      network_id: "*" // Match any network ID
+    }
   },
   // Configure your compilers
   compilers: {
